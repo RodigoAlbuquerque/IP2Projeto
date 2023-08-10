082aa65ca85d9e636fb84f5c00293371eaa62443
Rafael Rodrigues <rafael.rmsilva@ufrpe.br>
2023-08-09 21:25:02 -0300

    teste 6

--- farmaciarural/src/codigoFarmacia/App.java
+++ farmaciarural/src/codigoFarmacia/App.java
@@ -3,9 +3,11 @@ package codigoFarmacia;
 import codigoFarmacia.controle.ControladorCadastro;
 import codigoFarmacia.models.Cliente;
 import codigoFarmacia.models.Funcionario;
+import codigoFarmacia.models.Pessoa;
 import codigoFarmacia.models.Produto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public class App {
@@ -14,7 +16,11 @@ public class App {
 
         controladorCadastro.cadastrarPessoa(new Funcionario("rodrigodogera@gmail.com", "Rodrigo escamoso",
                                         "147895564", LocalDateTime.now(), 123456, 10));
-        controladorCadastro.cadastrarPessoa(new Funcionario("ra faelmcc@gmail.com", "Rafael platinado",
+        controladorCadastro.cadastrarPessoa(new Funcionario("canetaazul@gmail.com", "Manuel gomes",
+                                        "457933467", LocalDateTime.now(), 145793, 18));
+        controladorCadastro.cadastrarPessoa(new Funcionario("sobrevivendonaroca@gmail.com", "Erivelton",
+                                        "458763664", LocalDateTime.now(), 147958, 50));
+        controladorCadastro.cadastrarPessoa(new Funcionario("rafaelmcc@gmail.com", "Rafael platinado",
                                         "546497253", LocalDateTime.now(), 147859, 15));
 
         controladorCadastro.cadastrarProduto(new Produto("Fluviral", 12.5, 10, false));
@@ -22,11 +28,16 @@ public class App {
 
         controladorCadastro.cadastrarPessoa(new Cliente("alanfogueteiro@gmail.com", "Alan ratao",
                                         "142536789", LocalDateTime.now()));
+        controladorCadastro.cadastrarPessoa(new Cliente("guetoegueto@gmail.com", "Marcinho do gera",
+                                        "457983465", LocalDateTime.now()));
+        controladorCadastro.cadastrarPessoa(new Cliente("dalledalle@gmail.com", "MC anonimo",
+                                        "457985321", LocalDateTime.now()));
         controladorCadastro.cadastrarPessoa(new Cliente("josedelas@gmail.com", "Jose delas",
                                         "145795533", LocalDateTime.now()));
 
         controladorCadastro.descadastrarProduto("Fluviral");
         controladorCadastro.removerPessoa("546497253");
+        
 
         System.out.println("\n--------------------------------------------------------------");
         controladorCadastro.listarClientesQueMaisCompram();
