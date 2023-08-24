106ec7ec023adf34329554754a3898222373271f
jose-fernando-1 <107815035+jose-fernando-1@users.noreply.github.com>
2023-08-23 21:39:44 -0300

    Filtro data

---
+++ FarmaciaDaRural/src/main/java/GUI/FiltroData.java
@@ -0,0 +1,9 @@
+package GUI;
+import java.util.*;
+
+public class FiltroData {
+    public static void main(String[] args) {
+        Calendar data = Calendar.getInstance();
+        System.out.println("data:  " + data.getTime());
+    }
+}
